c634d013bc6aa0397f4d3f043da45aa124345b39
DinaSolitah <76052947+DinaSolitah@users.noreply.github.com>
2022-10-09 22:40:10 +0200

    Add files via upload

--- js/jquery.slicknav.js
+++ js/jquery.slicknav.js
@@ -43,11 +43,6 @@
 
     function Plugin(element, options) {
         this.element = element;
-
-        // jQuery has an extend method which merges the contents of two or
-        // more objects, storing the result in the first object. The first object
-        // is generally empty as we don't want to alter the default options for
-        // future instances of the plugin
         this.settings = $.extend({}, defaults, options);
 
         // Don't remove IDs by default if duplicate is false
